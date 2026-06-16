411f030eb47d52432038236a2addd942be482a94
Sudarshan Rane <ranesudarshan680@gmail.com>
2026-06-16 14:56:12 +0530

    Made changes in the repo structure to organise code daily efficiently

--- src/month1/day1_syntax/Main.java
+++ src/month1/day1_syntax/Main.java
@@ -1,9 +1,13 @@
+package month1.day1_syntax;
+
 public class Main {
 
     public static void main(String[] args){
 
         System.out.println("I like pizza");
         System.out.println("This is my first time learning java!!");
+        System.out.println("Git setup done!!!! hurray");
+        System.out.println("Pushed my code to git as well");
     }
 
 
