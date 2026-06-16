ade5b3f24844838354b13099ad9ddcba9a27a195
Sudarshan Rane <ranesudarshan680@gmail.com>
2026-06-16 14:40:41 +0530

    Git push test

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("I like pizza");
+        System.out.println("This is my first time learning java!!");
+    }
+
+
+}
