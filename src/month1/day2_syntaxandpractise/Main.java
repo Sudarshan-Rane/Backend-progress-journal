6acaa0a0c4791c2650ab014a26350e3167866b44
Sudarshan Rane <ranesudarshan680@gmail.com>
2026-06-17 15:31:21 +0530

    Feat: Day2 learnt about variables and its two types and started with the input feature with the scanner class

---
+++ src/month1/day2_syntaxandpractise/Main.java
@@ -0,0 +1,29 @@
+package month1.day2_syntaxandpractise;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args){
+//        System.out.println("Java is a great language and its greatly used in FINTECH companies widely");
+
+        /*int id = 10;
+        double percentage = 99.64;
+        char sign = '%';
+        boolean isEligible =true;
+        if (isEligible){
+            System.out.println("Student with the id "+id+" has achieved a total percentage of "+percentage+sign+" and is the class topper.");
+        }
+
+        String tower = "odosfkmdasdfsako tower";
+        System.out.println("The tower name is "+tower+ "this tower is the height of "+percentage+" meters talls. Making it one of the most tallest towers in Tokyo");
+*/
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("welkommen "+name);
+
+        scanner.close();
+    }
+}
