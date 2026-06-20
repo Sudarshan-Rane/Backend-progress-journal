9d3f6703951c263e38143b49dfbea89d214fbc58
Sudarshan Rane <ranesudarshan680@gmail.com>
2026-06-20 20:20:42 +0530

    feat: Day4 learnt ternary operator - did a exercise on ternary operator - learnt enhanced switches and did an exercise on it - learnt logical operators and did an exercise on it - learnt while loops and did an exercise on it

--- src/month1/day5/Main.java
+++ src/month1/day5/Main.java
@@ -39,10 +39,7 @@ public class Main {
 
 */
 
-
-        Scanner scanner = new Scanner(System.in);
-
-        int clearance;
+        /*  int clearance;
         boolean compliant;
         boolean VPN;
         boolean iP;
@@ -59,11 +56,87 @@ public class Main {
 
         System.out.print("iP: ");
         iP = scanner.nextBoolean();
-        
+
 
         flag  =(!iP && compliant) && ((clearance == 5) || ((clearance==3 || clearance==4)&&(VPN)) );
 
-        System.out.println("Access Granted: "+flag);
+        System.out.println("Access Granted: "+flag);*/
+
+        /*        double stock;
+        int win;
+        boolean open;
+        boolean voltile;
+        boolean executeTrade = false;
+
+        System.out.print("Stock: ");
+        stock = scanner.nextDouble();
+
+        System.out.print("Win: ");
+        win = scanner.nextInt();
+
+        System.out.print("Open: ");
+        open = scanner.nextBoolean();
+
+        System.out.print("Voltile: ");
+        voltile = scanner.nextBoolean();
+
+        executeTrade = ((open && stock > 0) && ((!voltile && win>=70) || (voltile && win>90 && stock <50)));
+
+        System.out.println("Execute Trade: "+executeTrade);*/
+
+        /*System.out.print("\n----ATM----\nEnter four digit pin: ");
+        int range = scanner.nextInt();
+
+        do {
+            System.out.print("Enter range again: ");
+            range = scanner.nextInt();
+        }while(range.);
+        System.out.print("GG");*/
+
+        /*int secretpin;
+        int guess=1234;
+        int attempts=3;
+
+        System.out.print("Enter your ATM pin: ");
+        secretpin = scanner.nextInt();
+
+        while(guess!=secretpin && attempts >= 1){
+            System.out.println("INCORRECT PIN");
+            System.out.println("You have "+attempts+" left");
+            System.out.print("Enter your ATM pin again: ");
+            secretpin = scanner.nextInt();
+            attempts--;
+        }
+        if (attempts == 0 ){
+            System.out.println("Account Locked. Too many failed attempts.");
+        }
+        else {
+            System.out.println("Access Granted! Welcome to your account.");
+        }*/
+
+
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.print("Enter number 1: ");
+        int number = scanner.nextInt();
+
+        System.out.print("Enter number 2: ");
+        int number2 = scanner.nextInt();
+
+        while(number <= 0 || number2 <= 0) {
+
+            System.out.println("----Enter positive number for both----");
+
+            System.out.print("Enter number 1: ");
+            number = scanner.nextInt();
+
+            System.out.print("Enter number 2: ");
+            number2 = scanner.nextInt();
+        }
+
+        System.out.println("Sum = " +(number+number2));
+
 
         scanner.close();
     }
