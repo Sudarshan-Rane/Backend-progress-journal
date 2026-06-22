0896d984c97cd12ce4fcb415a8210ee3e4c39b97
Sudarshan Rane <ranesudarshan680@gmail.com>
2026-06-22 21:21:28 +0530

    feat: Practise the past concepts with an exercise.

--- src/month1/day7_Methods_and_Overloading_methods_practise/Main.java
+++ src/month1/day7_Methods_and_Overloading_methods_practise/Main.java
@@ -1,10 +1,11 @@
 package month1.day7_Methods_and_Overloading_methods_practise;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
-    public static void main (String[]args){
+/*    public static void main (String[]args){
 
         Scanner scanner =  new Scanner(System.in);
 
@@ -33,6 +34,47 @@ public class Main {
 
     public static void printReceipt(String weaponName, int finalDamage){
         System.out.printf("%n--- CRAFTING COMPLETE ---%nWeapon: %s%nTotal Damage: %d", weaponName, finalDamage);
+    }*/
+
+    public static void main (String[]args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int code = random.nextInt(1,101);
+        int attempts = 5;
+
+        while(attempts>=1){
+            System.out.print("You have "+attempts+" attempts left. Enter code (1-100):");
+            int guess = scanner.nextInt();
+
+            if (guess == code) {
+                System.out.println("OVERRIDE ACCEPTED. Meltdown averted!");
+                break;
+            }
+            else{
+                if (guess>code){
+                    System.out.println("Access Denied. Target code is LOWER");
+                }
+                else {
+                    System.out.println("Access Denied. Target code is HIGHER");
+                }
+
+            }
+            attempts--;
+
+        }
+
+        if (attempts==0) {
+            System.out.println("SYSTEM LOCKDOWN. Meltdown initiated. The code was: "+code);
+        }
+        scanner.close();
+
+
+
+
+
+
     }
 
 
