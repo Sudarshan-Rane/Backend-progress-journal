ff8d6fd181f2d714c79bb4c354c40fa2c5f77724
Sudarshan Rane <ranesudarshan680@gmail.com>
2026-07-20 20:57:54 +0530

    feat: Learnt write.

--- src/month1/day20_Exceptions/Main.java
+++ src/month1/day20_Exceptions/Main.java
@@ -1,13 +1,16 @@
 package month1.day20_Exceptions;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.FileWriter;
 
 public class Main {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        try{
+
+       /* try{
             System.out.print("Enter your 4-digit PIN: ");
             int pin = scanner.nextInt();
             System.out.println("Access Granted for PIN: "+pin);
@@ -18,6 +21,21 @@ public class Main {
         finally {
             System.out.println("--- System Connection Closed ---");
             scanner.close();
+        }*/
+
+
+        String filepath = "C:\\Users\\sudar\\Downloads\\mission_log.txt";
+        String text = "Agent 007 Status Report\nThe target has been secured.";
+
+        try(FileWriter writer = new FileWriter(filepath,true)){
+            writer.write(text);
+            writer.close();
+            System.out.println("Log successfully written to hard drive.");
         }
+        catch (IOException e){
+            System.out.println("SYSTEM ERROR: Hard drive write failed.");
+            e.printStackTrace();
+        }
+
     }
 }
